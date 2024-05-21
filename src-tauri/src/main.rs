a39b0c476b921df3def8fcc3569a056353d366e3
brayo <vukubrian@gmail.com>
2024-05-21 18:34:48 +0300

    feat: temporary save point

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,10 @@ use lazy_static::lazy_static;
 use std::path::PathBuf;
 use std::sync::{Arc, Condvar, Mutex, OnceLock};
 
-use tauri::{AppHandle, Manager, SystemTray, SystemTrayEvent};
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTraySubmenu};
+use tauri::{AppHandle, Manager};
+// use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTraySubmenu};
+use tauri::tray::{ClickType, TrayIconBuilder};
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
 
 use aw_server::endpoints::build_rocket;
 
@@ -48,8 +50,8 @@ fn main() {
 
     let device_id = aw_server::device_id::get_device_id();
     let manager_state = manager::start_manager();
-    let tray = create_tray(&manager_state);
-    tauri::Builder::default()
+    // let tray = create_tray(&manager_state);
+    let _ = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             init_app_handle(app.handle().clone());
@@ -77,95 +79,118 @@ fn main() {
             };
 
             tauri::async_runtime::spawn(build_rocket(server_state, config).launch());
+            let toggle = MenuItemBuilder::with_id("toggle", "Toggle").build(app)?;
+            let menu = MenuBuilder::new(app).items(&[&toggle]).build()?;
+            let tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .on_menu_event(move |app, event| match event.id().as_ref() {
+                    "toggle" => {
+                        println!("toggle clicked");
+                    }
+                    _ => (),
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if event.click_type == ClickType::Left {
+                        let app = tray.app_handle();
+                        if let Some(webview_window) = app.get_webview_window("main") {
+                        let _ = webview_window.show();
+                        let _ = webview_window.set_focus();
+                        }
+                    }
+                })
+                .build(app).expect("Failed to build tray icon");
+
             Ok(())
-        })
-        .system_tray(tray)
-        .on_system_tray_event(move |app, event| on_tray_event(app, event, &manager_state))
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
-                event.window().hide().unwrap();
-                api.prevent_close();
-            }
-            _ => {}
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .build(tauri::generate_context!())
-        .expect("error while running tauri application")
-        .run(|_app_handle, event| match event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
-            _ => {}
         });
-}
-
-fn create_tray_menu(manager_state: &Arc<Mutex<manager::ManagerState>>) -> SystemTrayMenu {
-    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-    let open = CustomMenuItem::new("open".to_string(), "Open");
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-
-    // modules
-    let mut module_menu = SystemTrayMenu::new();
-
-    let state = manager_state.lock().unwrap();
-    println!("state: {:?}", state);
-    for (module, running) in state.modules_running.iter() {
-        let label = format!(
-            "{} ({})",
-            module,
-            if *running { "Running" } else { "Stopped" }
-        );
-        module_menu = module_menu.add_item(CustomMenuItem::new(module.clone(), &label));
     }
+//         .system_tray(tray)
+//         .on_system_tray_event(move |app, event| on_tray_event(app, event, &manager_state))
+//         .on_window_event(|event| match event.event() {
+//             tauri::WindowEvent::CloseRequested { api, .. } => {
+//                 event.window().hide().unwrap();
+//                 api.prevent_close();
+//             }
+//             _ => {}
+//         })
+//         .invoke_handler(tauri::generate_handler![greet])
+//         .build(tauri::generate_context!())
+//         .expect("error while running tauri application")
+//         .run(|_app_handle, event| match event {
+//             tauri::RunEvent::ExitRequested { api, .. } => {
+//                 api.prevent_exit();
+//             }
+//             _ => {}
+//         });
+// }
 
-    let module_submenu = SystemTraySubmenu::new("Modules", module_menu);
+// fn create_tray_menu(manager_state: &Arc<Mutex<manager::ManagerState>>) -> SystemTrayMenu {
+//     // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+//     let open = MenuItem::new(app, "Open", true, None::<&str>).unwrap();
+//     let quit = MenuItem::new(app, "Quit", true, None::<&str>).unwrap();
 
-    SystemTrayMenu::new()
-        .add_item(open)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_submenu(module_submenu)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit)
-}
 
-fn create_tray(manager_state: &Arc<Mutex<manager::ManagerState>>) -> SystemTray {
-    let tray_menu = create_tray_menu(manager_state);
-    SystemTray::new().with_menu(tray_menu)
-}
+//     // modules
+//     let mut module_menu = SystemTrayMenu::new();
 
-fn on_tray_event(
-    app: &AppHandle,
-    event: SystemTrayEvent,
-    manager_state: &Arc<Mutex<manager::ManagerState>>,
-) {
-    match event {
-        SystemTrayEvent::DoubleClick {
-            position: _,
-            size: _,
-            ..
-        } => {
-            println!("system tray received a double click");
-            let window = app.get_window("main").unwrap();
-            window.show().unwrap();
-        }
-        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-            "quit" => {
-                println!("system tray received a quit click");
-                let state = manager_state.lock().unwrap();
-                state.stop_modules();
-                app.exit(0);
-            }
-            "open" => {
-                println!("system tray received a open click");
-                let window = app.get_window("main").unwrap();
-                window.show().unwrap();
-            }
-            _ => {
-                println!("system tray received a module click at {}", id.as_str());
-                let mut state = manager_state.lock().unwrap();
-                state.handle_system_click(id.as_str());
-            }
-        },
-        _ => {}
-    }
-}
+//     let state = manager_state.lock().unwrap();
+//     println!("state: {:?}", state);
+//     for (module, running) in state.modules_running.iter() {
+//         let label = format!(
+//             "{} ({})",
+//             module,
+//             if *running { "Running" } else { "Stopped" }
+//         );
+//         module_menu = module_menu.add_item(CustomMenuItem::new(module.clone(), &label));
+//     }
+
+//     let module_submenu = SystemTraySubmenu::new("Modules", module_menu);
+
+//     SystemTrayMenu::new()
+//         .add_item(open)
+//         .add_native_item(SystemTrayMenuItem::Separator)
+//         .add_submenu(module_submenu)
+//         .add_native_item(SystemTrayMenuItem::Separator)
+//         .add_item(quit)
+// }
+
+// fn create_tray(manager_state: &Arc<Mutex<manager::ManagerState>>) -> TrayIconBuilder<_> {
+//     let tray_menu = create_tray_menu(manager_state);
+//     TrayIconBuilder::new().menu(&tray_menu)
+// }
+
+// fn on_tray_event(
+//     app: &AppHandle,
+//     event: SystemTrayEvent,
+//     manager_state: &Arc<Mutex<manager::ManagerState>>,
+// ) {
+//     match event {
+//         SystemTrayEvent::DoubleClick {
+//             position: _,
+//             size: _,
+//             ..
+//         } => {
+//             println!("system tray received a double click");
+//             let window = app.get_window("main").unwrap();
+//             window.show().unwrap();
+//         }
+//         SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+//             "quit" => {
+//                 println!("system tray received a quit click");
+//                 let state = manager_state.lock().unwrap();
+//                 state.stop_modules();
+//                 app.exit(0);
+//             }
+//             "open" => {
+//                 println!("system tray received a open click");
+//                 let window = app.get_window("main").unwrap();
+//                 window.show().unwrap();
+//             }
+//             _ => {
+//                 println!("system tray received a module click at {}", id.as_str());
+//                 let mut state = manager_state.lock().unwrap();
+//                 state.handle_system_click(id.as_str());
+//             }
+//         },
+//         _ => {}
+//     }
+// }
